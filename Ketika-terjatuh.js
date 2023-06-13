3705f0e1fc9f929d2db2bc0e29f5fef6c9d3af35
QaruzDin <fiqartsam.epsilonphi@gmail.com>
2023-06-13 12:42:32 +0700

    Memasukkan berkas html dan js

---
+++ Ketika-terjatuh.js
@@ -0,0 +1,8 @@
+let terjatuh = confirm('Apakah kamu terjatuh?')
+
+while(terjatuh == true){
+    alert('Bangkit lagi.');
+    terjatuh = confirm('Apakah kamu terjatuh?');
+}
+
+alert('Alhamdulillah, kamu sudah bangkit ^_^')
